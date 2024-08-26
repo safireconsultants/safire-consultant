36b6932469b33d4b9da128dd8843f988fe2e4448
Atmajo <atmajoc@gmail.com>
2024-08-26 23:57:04 +0530

    refactor: Update Review component layout and content
    - Update profile image to skycap-man.jpeg
    - Update job title to Principal Consultant
    - Update company name to @Skycap PTE.LTD
    - Update description to highlight partnership with Safire Consultants

--- components/Review/Review.tsx
+++ components/Review/Review.tsx
@@ -3,16 +3,25 @@ import React from "react";
 
 const Review = () => {
   return (
-    <div className="flex flex-col justify-center space-y-4 bg-white rounded-lg p-5 text-black w-64">
+    <div className="flex flex-col justify-center space-y-4 bg-white rounded-lg p-5 text-black w-80">
       <div className="flex items-center space-x-4">
-        <Image src="/profile.png" alt="profile" width={100} height={100} className="w-14" />
+        <Image
+          src="/skycap-man.jpeg"
+          alt="skycap-man"
+          width={100}
+          height={100}
+          className="w-14 rounded-full"
+        />
         <div>
-          <h1 className="text-lg font-semibold">John Doe</h1>
-          <p className="text-sm">CEO</p>
+          <h1 className="text-lg font-semibold">Principal Consultant</h1>
+          <p className="text-sm">@Skycap PTE.LTD</p>
         </div>
       </div>
       <div>
-        <p>Journeying through the world, Chillhum always with me</p>
+        <p>
+          Partnering with Safire Consultants was a game-changer for our business
+          success.
+        </p>
       </div>
     </div>
   );
